9946362cd2e667219b3dedce8dc43406471e41c0
BrazhnikDA <dimity.brazhnik@yandex.ru>
2021-04-26 18:11:22 +0300

    Добавьте файлы проекта.

---
+++ List_two_arrays/main.cpp
@@ -0,0 +1,59 @@
+#include "List.h"
+#include <stdio.h>
+#include <locale>
+
+#define PATH "C:\\out.txt"
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	std::cout << "\nТестирование функционала: \n";
+	List list1;
+	List list2;
+
+	List s;
+
+	s.InsFirst(15);
+	s.InsFirst(3);
+	s.InsFirst(-2);
+	s.InsFirst(0);
+	s.InsFirst(0);
+	s.InsFirst(0);
+	s.InsFirst(11);
+	s.InsFirst(6);
+	s.InsFirst(9);
+
+	//s.InsLast(010101);
+
+	std::cout << "Исходный список: ";
+	s.print();
+
+
+	list1.InsFirst(15);
+	list1.InsFirst(9);
+	list1.InsFirst(33);
+	list1.InsFirst(66);
+
+	list1.WriteFile(PATH);
+
+	list2.ReadFile(PATH);
+	list2.print();
+
+	list2.InsFirst(1);
+	list2.InsFirst(6);
+	list2.InsFirst(7);
+	list2.InsFirst(9);
+	list2.InsFirst(2);
+	list2.InsFirst(333);
+
+	list1.print();
+
+	list2.print();
+
+	List find;
+	find = *list2.findSpecialElements(3);
+	find.print();
+
+	return 0;
+}
